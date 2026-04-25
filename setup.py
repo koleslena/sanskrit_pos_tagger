006f820fb601364d00db2b7b25726be7d4e6895f
Elena Kolesnikova <koleslena@yandex.ru>
2026-04-25 15:03:10 +0300

    new version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(os.path.join(here, 'requirements.txt')) as f:
 
 setup(
     name='sanskrit_tagger',
-    version='0.1.5',
+    version='0.1.6',
     include_package_data=True,
     package_data={
         'sanskrit_tagger': ['data/*.dat'],
