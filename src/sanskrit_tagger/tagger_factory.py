412092b6df4bc3e99f0e237190476a9e1363a793
Elena Kolesnikova <koleslena@yandex.ru>
2026-05-02 21:42:50 +0300

    fixed tags

--- src/sanskrit_tagger/tagger_factory.py
+++ src/sanskrit_tagger/tagger_factory.py
@@ -2,7 +2,7 @@ from sanskrit_tagger.pos_tagger import POSTagger
 from sanskrit_tagger.segmenter import Segmenter
 
 def get_pos_tagger(model, **kwargs):
-    return POSTagger(model, model.char2id, model.tags, **kwargs)
+    return POSTagger(model, model.char2id, model.unique_tags, **kwargs)
 
 def get_segmenter(model, **kwargs):
     return Segmenter(model, model.char2id, **kwargs)
